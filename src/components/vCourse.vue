9608c636dd08110c2b54d3dfe46fe8635a96e889
termokop <termokop564@gmail.com>
2023-01-15 01:42:44 +0400

    add_icons_for_course

--- src/components/vCourse.vue
+++ src/components/vCourse.vue
@@ -3,23 +3,52 @@
     <h1 class='center'>Курс картвельської мови</h1> <br>
     <div class="studyMenu">
         <button class="element"
+            :disabled="user_progress + 3 < n * 3"
             v-for="n in 10"
             :key="n"
-            @click.prevent="startLesson(n)">
-            <p>Урок {{ n }}</p>
+            >
+            <p class="center">Урок {{ n }}</p>
+            <div class="buttons">
+                <button
+                    :disabled="user_progress + 3 < n * 3"
+                    @click.prevent="startLesson(n)" 
+                    class="theory"
+                    >
+                    <img src="../assets/theory.svg" alt="theory">
+                    <img v-if="user_progress+3 > n * 3 + 0" class="done" src="../assets/done.svg" alt="done">
+                </button>
+                <button 
+                    @click.prevent="testFn" 
+                    :disabled="user_progress + 3 < n * 3"
+                    class="practice"
+                    >
+                    <img src="../assets/practice_lesson.svg" alt="practice">
+                    <img v-if="user_progress+3 > n * 3 + 1" class="done" src="../assets/done.svg" alt="done">
+                </button>
+                <button 
+                    :disabled="user_progress + 3 < n * 3"
+                    class="exam"
+                    >
+                    <img src="../assets/doneX.svg" alt="doneExam">
+                    <img v-if="user_progress+3 > n * 3 + 2" class="done" src="../assets/done.svg" alt="done">
+                </button>
+            </div>
         </button>
     </div>
 </div>
 
-    <vLesson 
-        v-if="isLessonOpened" 
-        :lessonObj="lessonObj"
-        @backToMenu="isLessonOpened = false"
-        @start_quiz="start_quiz"
-        >
-    </vLesson>
+<vLesson 
+    v-if="isLessonOpened" 
+    :lessonObj="lessonObj"
+    @backToMenu="isLessonOpened = false"
+    @start_quiz="start_quiz"
+    >
+</vLesson>
 
-    <vLoader :loader="loader"></vLoader>
+<vLoader 
+    :loader="loader"
+    >
+</vLoader>
 
 
 </template>
@@ -42,13 +71,10 @@ export default {
     data () {
         return {
             dictionary,
-            topics: null,
             loader: false,
-            quiz: null,
-            openQuiz: false,
-            lastTopic: '',
             lessonObj: null,
-            isLessonOpened: false
+            isLessonOpened: false,
+            user_progress: +localStorage.getItem('progress_in_course'),
         }
     },
     methods: {
@@ -56,6 +82,7 @@ export default {
             this.$emit('start_quiz', arr)
         },
         async startLesson(lesson) {
+            this.change_user_progress((lesson-1)*3+1)
             this.loader = true
             const url = 'https://www.ukrge.site/api/get_lesson.php'
             const json = JSON.stringify({lesson: lesson})
@@ -76,12 +103,18 @@ export default {
             } finally {
             this.loader = false
             }
+        },
+        change_user_progress(n) {
+            let current = localStorage.getItem('progress_in_course')
+            localStorage.setItem('progress_in_course', n > current ? n : current)
+        },
+        testFn() {
+            console.log(this.user_progress)
         }
     },
-    mounted() {
-
+    updated() {
+        this.user_progress = +localStorage.getItem('progress_in_course');
     },
-
 }
 
 </script>
@@ -90,7 +123,6 @@ export default {
 h1 {
     color: #fff;
 }
-
     .studyMenu {
         display: flex;
         width: 100%;
@@ -113,10 +145,9 @@ h1 {
         color: #fff;
         align-self: flex-start;
         overflow:hidden;
-        scrollbar-color: red;
         height: 150px;
     }
-    .element:hover{
+    button.element:hover{
         border-width: 3px 3px 3px 3px;
     }
 
@@ -125,6 +156,39 @@ h1 {
         background-color: gray;
     }
 
+    p {
+        padding: 10px;
+    }
+
+
+    .buttons {
+        height: 70%;
+        display: flex;
+        justify-content: space-between;
+        align-content: center;
+
+    }
+
+    button {
+        position: relative;
+        width: 30%;
+        height: 100%;
+        margin: auto;
+        background-color: transparent;
+    }
+    .buttons button:hover {
+        background: #2d2d2d;
+    }
+
+    button img {
+        width: 100%;
+    }
+      img.done {
+        position: absolute;
+        right: 0;
+        bottom: 0;
+        width: 40%;
+    }
     @media screen and (max-width: 500px) {
         .studyMenu {
         width: 95vw;
